06305d5df0d86f7e3b341e3645595a184ad036dc
JardelMD <130687030+JardelMD@users.noreply.github.com>
2024-01-08 13:26:15 -0300

    feat: first commit

---
+++ Portfolio-Jardel/src/components/HomePage.jsx
@@ -0,0 +1,9 @@
+export const HomePage = () => {
+    return (
+        <>
+            <header>Teste header</header>
+            <main>Teste main</main>
+            <footer>Teste footer</footer>
+        </>
+    )
+}
