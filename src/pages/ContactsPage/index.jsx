3183041bb36463e468bc7808fc99ea05d4ceff7e
JardelMD <130687030+JardelMD@users.noreply.github.com>
2024-01-29 18:25:32 -0300

    feat: Adicionando contatos

--- src/pages/ContactsPage/index.jsx
+++ src/pages/ContactsPage/index.jsx
@@ -1,7 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./styled.module.scss";
+import { FaLinkedin, FaWhatsappSquare, FaArrowAltCircleRight } from "react-icons/fa";
+import { PiMicrosoftOutlookLogoFill } from "react-icons/pi";
+import { Footer } from "../../components/Footer";
+
+
 export const ContactsPage = () => {
+    const navigate = useNavigate();
+    const redirectToHome = () => {
+        navigate("/")
+    }
     return (
-        <>
-            <h1>Página de contatos</h1>
-        </>
+        <section className={styles.main}>
+            <header>
+                <h1>Jardel Moreira Dylewski</h1>
+                <button onClick={redirectToHome}>Voltar</button>
+            </header>
+            <div>
+                <h1>Meus contatos:</h1>
+                <ul>
+                    <li>
+                        <h2>LinkedIn: </h2>
+                        <a href="https://www.linkedin.com/in/jardel-moreira-dylewski-a965a1272/" target="_blank"> <FaLinkedin size={50} color="#0a66c2" /></a>
+                    </li>
+                    <li>
+                        <h2>Email: </h2>
+                        <a href="#" title={'jardeldylewski@hotmail.com'} ><PiMicrosoftOutlookLogoFill size={50} color="#0078d4" /> </a>
+                        <span>jardeldylewski@hotmail.com</span>
+                    </li>
+                    <li>
+                        <h2>WhatsApp: </h2>
+                        <a href="https://wa.me/+5551999567434" target="_blank"><FaWhatsappSquare size={50} color="#00a884" /></a>
+                    </li>
+                </ul>
+            </div>
+            <Footer />
+        </section>
     )
 }
