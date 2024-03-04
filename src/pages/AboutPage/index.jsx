90e190ed171ef6b60cb5b67e722f5a091debe1f5
JardelMD <130687030+JardelMD@users.noreply.github.com>
2024-03-04 15:54:21 -0300

    feat: projects and about me

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -21,8 +21,17 @@ export const AboutMePage = () => {
                 <div>
                     <h1>Sobre mim:</h1>
                     <p>Olá, me chamo Jardel Moreira Dylewski. Sou natural de Dom Feliciano-RS, mas moro em Porto Alegre-RS há alguns anos.
-                        Sou formado em Licenciatura em Matemática pela Universidade Federal de Pelotas (UFPel), possuo Mestrado em Matemática Aplicada e Doutorado em Matemática Aplicada pela Universidade Federal do Rio Grande do Sul (UFRGS). Durante toda a minha formação tive contato com programação, desenvolvendo trabalhos e projetos de pesquisa. Após a conclusão do meu doutorado, conversando com alguns desenvolvedores formados, tive interesse em ingressar nessa promissora área, e aqui estou! Atualmente sou Desenvolvedor Front-End e estou cursando Back-End na <a className={styles.link} href="https://kenzie.com.br/">Kenzie Academy Brasil</a> para concluir meu curso de Desenvolvedor Web Fullstack.</p>
+                        Sou formado em Licenciatura em Matemática pela Universidade Federal de Pelotas (UFPel), possuo Mestrado em Matemática Aplicada e Doutorado em Matemática Aplicada pela Universidade Federal do Rio Grande do Sul (UFRGS). Durante toda a minha formação tive contato com programação, desenvolvendo trabalhos e projetos de pesquisa. Após a conclusão do meu doutorado, conversando com alguns desenvolvedores formados, tive interesse em ingressar nessa promissora área, e aqui estou! Atualmente sou Desenvolvedor Front-End e estou cursando Back-End na <a className={styles.link} href="https://kenzie.com.br/" target="_blank">Kenzie Academy Brasil</a> para concluir meu curso de Desenvolvedor Web Fullstack.</p>
                 </div>
+            </div>
+            <div className={styles.div}>
+                <h2>Minha jornada até agora:</h2>
+                <ul>
+                    <li>03/2013 - 02/2016: Licenciatura em Matemática (UFPel);</li>
+                    <li>03/2017 - 03/2019: Mestrado em Matemática Aplicada (UFRGS);</li>
+                    <li>04/2019 - 03/2023: Doutorado em Matemática Aplicada (UFRGS);</li>
+                    <li>05/2023 - atualmente: Desenvolvimento Web Fullstack (<a className={styles.link} href="https://kenzie.com.br/" target="_blank">Kenzie Academy Brasil</a>).</li>
+                </ul>
             </div>
             <Footer />
         </section>
